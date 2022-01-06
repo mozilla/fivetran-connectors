a751e9c64fc95a60f889741f39fcf795492c78e8
Alexander Nicholson <anicholson@mozilla.com>
2022-01-06 17:54:33 -0500

    DENG-57 - Updates based on API changes (#9)

--- connectors/casa/main.py
+++ connectors/casa/main.py
@@ -40,14 +40,13 @@ def main(request):
     fetch_more_projects = state.get("fetch_more_projects", True)
     fetch_more_users = state.get("fetch_more_users", True)
     users_offset = state.get("users_offset", 0)
-    projects_offset = state.get("projects_offset", 0)
+    projects_bookmark = state.get("projects_bookmark")
 
-    def _fetch(offset, url):
-        logging.info(f"Sending request to {url} with offset: {offset}")
-        request_params = {
-            "limit": QUERY_RESULT_LIMIT,
-            "offset": offset,
-        }
+    def _fetch(url, request_params=None):
+        request_params = request_params or {}
+        request_params["limit"] = QUERY_RESULT_LIMIT
+
+        logging.info(f"Sending request to {url} with params: {request_params}")
 
         headers = {"Authorization": f"Bearer {access_token}"}
 
@@ -58,34 +57,36 @@ def main(request):
                 f"Error connecting to Biztera API (url: {url}). Response: {http_response}"
             )
 
-        results = http_response.json()
-        if "data" in results:
-            results = results["data"]
+        return http_response.json()
 
-        if len(results) == QUERY_RESULT_LIMIT:
-            fetch_more = True
-            new_offset = offset + QUERY_RESULT_LIMIT
-        else:
-            fetch_more = False
-            new_offset = 0
-
-        return results, fetch_more, new_offset
+    projects = []
+    new_fetch_more_projects = False
+    new_projects_bookmark = None
 
     if fetch_more_projects:
-        projects, new_fetch_more_projects, new_projects_offset = _fetch(
-            projects_offset, PROJECTS_URL
-        )
-    else:
-        projects = []
-        new_fetch_more_projects = False
-        new_projects_offset = 0
+        if projects_bookmark is not None:
+            projects_response = _fetch(
+                PROJECTS_URL, request_params={"bookmark": projects_bookmark}
+            )
+        else:
+            projects_response = _fetch(PROJECTS_URL)
+
+        projects = projects_response["data"]
+        metadata = projects_response["metadata"]
+
+        if "nextPage" in metadata:
+            new_projects_bookmark = metadata["nextPage"]["bookmark"]
+            new_fetch_more_projects = True
+
+    users = []
+    new_fetch_more_users = False
+    new_users_offset = 0
 
     if fetch_more_users:
-        users, new_fetch_more_users, new_users_offset = _fetch(users_offset, USERS_URL)
-    else:
-        users = []
-        new_fetch_more_users = False
-        new_users_offset = 0
+        users = _fetch(USERS_URL, request_params={"offset": users_offset})
+        if len(users) == QUERY_RESULT_LIMIT:
+            new_fetch_more_users = True
+            new_users_offset = users_offset + QUERY_RESULT_LIMIT
 
     has_more = new_fetch_more_users or new_fetch_more_projects
 
@@ -93,14 +94,14 @@ def main(request):
     if not has_more:
         new_fetch_more_projects = True
         new_fetch_more_users = True
-        new_projects_offset = 0
+        new_projects_bookmark = None
         new_users_offset = 0
 
     new_state = {
         "fetch_more_users": new_fetch_more_users,
         "fetch_more_projects": new_fetch_more_projects,
         "users_offset": new_users_offset,
-        "projects_offset": new_projects_offset,
+        "projects_bookmark": new_projects_bookmark,
     }
 
     logging.info(
